2375a06606f5c36d6b22b463a539ee0f6db973dd
agentbellnorm <morgan.bentell@gmail.com>
2016-11-10 23:58:45 +0100

    unit tests

---
+++ src/java/algorithm/GainTuple.java
@@ -0,0 +1,7 @@
+package algorithm;
+
+/**
+ * Created by Morgan on 2016-11-09.
+ */
+public class GainTuple {
+}
---
+++ test/java/algorithm/ID3PerformerTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Morgan on 2016-11-09.
+ */
+public class ID3PerformerTest {
+
+}
